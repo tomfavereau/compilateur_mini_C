323160a189c1e2672701f59814f30345e8f5cc55
tomfavereau <144817639+tomfavereau@users.noreply.github.com>
2024-06-17 18:47:17 +0200

    Create exemple2.c

---
+++ exemple2.c
@@ -0,0 +1,9 @@
+main(X, Y) {
+                        var tX[X];
+                        printf(len(tX));
+                        tX[1] = 1;
+                        printf(tX[1]);
+                        X = tX[Y];
+                        printf(X);
+                        return(len(tX));
+                    }
